b9774c83f3f44ab6fd28f764a3dbd6ba5a235d32
Lousher <779546225@qq.com>
2025-06-18 15:39:59 +0800

    [template copy] create simplest template

---
+++ modules/dom.scm
@@ -0,0 +1,33 @@
+(define-module (dom)
+  #:use-module (hoot ffi)
+  #:re-export (procedure->external)
+  #:export (document-body
+	    console-log
+	    add-event-listener!
+	    text-content
+	    get-element-by-id
+	    insert-adjacent-HTML))
+
+(define-foreign document-body
+  "document" "body"
+  -> (ref null extern))
+
+(define-foreign console-log
+  "console" "log"
+  (ref string) -> none)
+
+(define-foreign add-event-listener!
+  "element" "addEventListener"
+  (ref null extern) (ref string) (ref null extern) -> none)
+
+(define-foreign text-content
+  "element" "textContent"
+  (ref null extern) (ref string) -> none)
+
+(define-foreign get-element-by-id
+  "document" "getElementById"
+  (ref string) -> (ref null extern))
+
+(define-foreign insert-adjacent-HTML
+  "element" "insertAdjacentHTML"
+  (ref null extern) (ref string) (ref string) -> (ref null extern))
