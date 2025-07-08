17e1881a5308422ba35513b80896d9e669663bc1
Lousher <779546225@qq.com>
2025-07-08 11:38:35 +0800

    [combine listeners and body.js]

--- body.js
+++ body.js
@@ -12,3 +12,21 @@ count.setAttribute("id", "count")
 document.body.appendChild(dec);
 document.body.appendChild(count);
 document.body.appendChild(inc);
+
+// copy from listeners.js
+const [num, setNum] = signal(0);
+
+const c = document.getElementById("count");
+
+c.textContent = num();
+
+effect(() => c.textContent= num());
+const incFun = () => { setNum(num() + 1); };
+const decFun = () => { setNum(num() - 1); };
+
+const d = document.getElementById("dec");
+const i = document.getElementById("inc");
+
+d.addEventListener("click", decFun);
+i.addEventListener("click", incFun);
+
