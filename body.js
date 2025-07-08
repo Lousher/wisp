3ee61eb3b6a10ca861f865707823b9e9b6c6125b
Lousher <779546225@qq.com>
2025-07-08 11:43:38 +0800

    [refactor the body.js]

--- body.js
+++ body.js
@@ -1,32 +1,20 @@
+const [num, setNum] = signal(0);
+
 const dec = document.createElement("button");
-dec.setAttribute("id","dec")
 dec.textContent = "Minus";
+const decFun = () => { setNum(num() - 1); };
+dec.addEventListener("click", decFun);
 
 const inc = document.createElement("button");
-inc.setAttribute("id","inc")
 inc.textContent = "Plus";
+const incFun = () => { setNum(num() + 1); };
+inc.addEventListener("click", incFun);
 
 const count = document.createElement("h1");
-count.setAttribute("id", "count")
+count.textContent = num();
+effect(() => count.textContent= num());
 
 document.body.appendChild(dec);
 document.body.appendChild(count);
 document.body.appendChild(inc);
-
-// copy from listeners.js
-const [num, setNum] = signal(0);
-
-const c = document.getElementById("count");
-
-c.textContent = num();
-
-effect(() => c.textContent= num());
-const incFun = () => { setNum(num() + 1); };
-const decFun = () => { setNum(num() - 1); };
-
-const d = document.getElementById("dec");
-const i = document.getElementById("inc");
-
-d.addEventListener("click", decFun);
-i.addEventListener("click", incFun);
 
