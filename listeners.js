dbddc2db02ac34827e7219110d36303ba427c4dd
Lousher <779546225@qq.com>
2025-07-08 11:04:18 +0800

    [simplest reactive counter]

--- listeners.js
+++ listeners.js
@@ -1,5 +1,16 @@
-const num = 0;
+var num = 0;
 
 const c = document.getElementById("count");
 
+
 c.textContent = 0;
+
+const incFun = () => { num += 1; c.textContent = num; };
+const decFun = () => { num -= 1; c.textContent = num; };
+
+const d = document.getElementById("dec");
+const i = document.getElementById("inc");
+
+d.addEventListener("click", decFun);
+i.addEventListener("click", incFun);
+
