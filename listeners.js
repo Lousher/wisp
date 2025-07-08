05e71904207728f925e760615f16fd2547abd693
Lousher <779546225@qq.com>
2025-07-08 11:15:41 +0800

    [introduce signal effect]

--- listeners.js
+++ listeners.js
@@ -5,8 +5,9 @@ const c = document.getElementById("count");
 
 c.textContent = num();
 
-const incFun = () => { setNum(num() + 1); c.textContent = num(); };
-const decFun = () => { setNum(num() - 1); c.textContent = num(); };
+effect(() => c.textContent= num());
+const incFun = () => { setNum(num() + 1); };
+const decFun = () => { setNum(num() - 1); };
 
 const d = document.getElementById("dec");
 const i = document.getElementById("inc");
